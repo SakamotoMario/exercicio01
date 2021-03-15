6c8efaf76bcaf2b6961acd55589ba3c8bca5cd0a
mario <mariosakkamoto@gmail.com>
2021-03-15 09:36:14 -0300

    operadores matematicos

---
+++ src/main/kotlin/nome.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    val x = 6
+    val y = 2
+
+    println("Operador soma")
+    println(x+y)
+
+    println("Operador subtracao")
+    println(x-y)
+    println(y-x)
+
+    println("Operador multiplicacao")
+    println(x*y)
+
+    println("Operador divisao")
+    println(x/y)
+
+
+}
